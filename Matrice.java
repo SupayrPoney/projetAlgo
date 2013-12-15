875efdf180db9f756395691adad83b27eafab151
Tio Nogueras Gerard <gerard@catalancoder>
2013-12-15 07:13:18 +0100

    Je comprends pas comment dans "IN MATRICE" je n'arrive pas à modifier la matrice

--- Matrice.java
+++ Matrice.java
@@ -2,38 +2,47 @@ import java.util.Vector;
 
 
 public class Matrice {
-	Vector <String> matrice [][];
-	public Matrice(Vector <String>Cercles, Vector <String>Dettes){
-		System.out.print("Cercles.size: ");
-		System.out.println(Cercles.size());
-		System.out.print("Dettes.size: ");
-		System.out.println(Cercles.size());
-		//Initialisation d'une matrice avec le nb de cercles et leur fonds(mais en 2 strings différents)
-		String listeCerclesEtFonds[][] = new String [Cercles.size()][2];
-		String listeCerclesEtDettes[][] = new String [Dettes.size()][3];
-		for(int i=0; i< Cercles.size(); i++){
-			//prend le string "CD 20" par exemple
-			String string1 = Cercles.get(i);
-			System.out.print("Cercle.get(i): ");
-			System.out.println(Cercles.get(i));
-			//On va récupérer le cercle et le fond
-			String[] CercleEtFond = string1.split(" ");
-			System.out.print("CercleEtFond: ");
-			System.out.println(CercleEtFond);
-			//placer ds une liste pour le récupérer plus facilements
-			listeCerclesEtFonds[i][0] = CercleEtFond[0];
-			listeCerclesEtFonds[i][1] = CercleEtFond[1];
-			for(int j=0; j< Dettes.size(); j++){
-				if(i==0){
-					//idem qu'au dessus mas pour les dettes
-					String string2 = Dettes.get(j);
-					String[] CercleEtDette = string2.split(" ");
-					listeCerclesEtDettes[j][0] = CercleEtDette[0];
-					listeCerclesEtDettes[j][1] = CercleEtDette[1];
-					listeCerclesEtDettes[j][2] = CercleEtDette[2];
+	//Attributs
+	private Vector<String> matrice [][];
+	//Constructeur
+	public Matrice(String Cercles[][], String Dettes[][]){
+		System.out.println("IN Matrice");
+		matrice = new Vector[Cercles.length][Dettes.length];
+		for(int i=0; i< Cercles.length; i++){
+			for(int j=0; j< Dettes.length; j++){
+				Vector<String> element = new Vector();
+				System.out.print("Dettes[0]");
+				System.out.println(Dettes[j][0]);
+				System.out.print("Dettes[1]");
+				System.out.println(Dettes[j][1]);
+				System.out.print("Cerles[0]");
+				System.out.println(Cercles[i][0]);
+				if(Cercles[i][0].equals(Dettes[j][0])){
+					//si c'est le début de l'arc
+					element.add("1");
+					element.add(Dettes[j][2]);
+					matrice[i][j] = element;
 				}
-				if()
+				else if(Cercles[i][0].equals(Dettes[j][1])){
+					//si c'est la fin de l'arc
+					element.add("-1");
+					element.add(Dettes[j][2]);
+					matrice[i][j] = element;
+				}
+				else{
+					// si l'arc ne passe pas par ces sommets
+					element.add("0");
+					element.add("0");
+					matrice[i][j] = element;
+				}
 			}
 		}
+		for(int i=0; i< Cercles.length; i++){
+			for(int j=0; j< Dettes.length; j++){
+				System.out.print(matrice[i][j]);
+			}
+			System.out.println(",");
+		}
 	}
+	//Méthodes
 }
