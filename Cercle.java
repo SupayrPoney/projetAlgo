a59a7bc710da81a710b448d06b877bb8a99a3189
ROCHA PEREIRA Bruno <brochape@ulb.ac.be>
2013-12-17 19:56:46 +0100

    Added attribute "numero" and getter

--- Cercle.java
+++ Cercle.java
@@ -3,15 +3,23 @@ import java.util.Vector;
 
 public class Cercle {
 	//Attributs
-	private int solde;
+	private int solde_;
+	private int num;
 
+	public Cercle(int numero,int solde){
+		solde_ = solde;
+		num = numero;
+	}
 	public void augmenterSolde(int montant){
-		solde += montant;
+		solde_ += montant;
 	}
 	public void diminuerSolde(int montant){
-		solde -= montant;
+		solde_ -= montant;
 	}
-	public int getsolde(){
-		return solde;
+	public int getNum(){
+		return num;
+	}
+	public int getSolde(){
+		return solde_;
 	}
 }
