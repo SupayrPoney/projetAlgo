39c5df541a024c875f3de9598c247b9068ac1e45
ROCHA PEREIRA Bruno <brochape@ulb.ac.be>
2013-12-17 16:38:36 +0100

    Created Relation.java

---
+++ Relation.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+
+public class Relation{
+	private Cercle from;
+	private Cercle to;
+	private int montantDette;
+
+	public Cercle getFrom(){
+		return from;
+	}
+	public Cercle getTo(){
+		return to;
+	}
+
+}
